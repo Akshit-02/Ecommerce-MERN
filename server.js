da231533dad89d6759bda797f723ed23b8adbfd7
Akshit-02 <akshitmurarka2002@gmail.com>
2024-06-16 12:22:37 +0530

    project init and setup

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express" 
+import dotenv from "dotenv"
+import morgan from "morgan"
+import connectDB from "./config/db.js";
+
+// config env
+dotenv.config();
+
+//database config
+connectDB();
+
+// rest object
+const app=express();
+
+//middleware
+app.use(express.json())
+app.use(morgan("dev"))
+
+//rest api
+app.get("/",(req,res)=>{
+    res.send("<h1>Welcome to ecommerce app");
+});
+
+const PORT=process.env.PORT || 8080;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`)
+})
