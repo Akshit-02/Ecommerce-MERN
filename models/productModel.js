2688054d7f3e77417f8d092e3ef3b3b493cd0138
Akshit-02 <akshitmurarka2002@gmail.com>
2024-06-16 18:28:32 +0530

    fixes

---
+++ models/productModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const productSchema=new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    slug:{
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    category:{
+        type: mongoose.ObjectId,
+        ref: 'category',
+    }
+})
